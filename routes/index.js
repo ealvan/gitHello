814c97f5e6483c0382fc7f1cacdbd176d1319c08
edsel <ealvan@unsa.edu.pe>
2022-04-08 21:49:27 -0500

    update repo

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,9 @@
 var express = require('express');
 var router = express.Router();
-
+var platform = ["Linux", "Windows", "Unix/MacOS"];
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: platform[0] });
 });
-
+console.log("You're running on: ", process.platform);
 module.exports = router;
